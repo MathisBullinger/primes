c4a5a7e3ff8b0407b30aa12aff9ff68d77b8c878
Mathis Bullinger <mathis@bullinger.dev>
2021-02-19 22:00:12 +0000

    feat: resize & zoom

--- src/index.ts
+++ src/index.ts
@@ -3,32 +3,50 @@ const ctx = canvas.getContext('2d')
 
 setSize()
 
-// axes
-ctx.strokeStyle = '#fff8'
-ctx.lineWidth = 0.5
-ctx.moveTo(0, canvas.height / 2)
-ctx.lineTo(canvas.width, canvas.height / 2)
-ctx.stroke()
-ctx.moveTo(canvas.width / 2, 0)
-ctx.lineTo(canvas.width / 2, canvas.height)
-ctx.stroke()
+function render() {
+  ctx.fillStyle = "#111"
+  ctx.fillRect(0, 0, canvas.width, canvas.height)
+  renderAxes()
+  ctx.fillStyle = "#ff06"
+  for (let i = 1; i < 10000; i++) renderNum(i)
+}
 
-ctx.fillStyle = '#ff08'
-const scale = 1
+let scale = 1
+
+function renderAxes() {
+  ctx.strokeStyle = '#fff'
+  ctx.lineWidth = 0.5
+  ctx.moveTo(0, canvas.height / 2)
+  ctx.lineTo(canvas.width, canvas.height / 2)
+  ctx.stroke()
+  ctx.moveTo(canvas.width / 2, 0)
+  ctx.lineTo(canvas.width / 2, canvas.height)
+  ctx.stroke()
+}
 
 function renderNum(num: number) {
   const dist = num * scale
   const x = Math.sin(num) * dist
   const y = Math.cos(num) * dist
-  
+
   ctx.beginPath()
   ctx.arc(canvas.width / 2 + x, canvas.height / 2 + y, 2 * devicePixelRatio, 0, Math.PI * 2)
   ctx.fill()
 }
 
-for (let i = 0; i < 1000; i++) renderNum(i)
-
 function setSize() {
   canvas.width = window.innerWidth * devicePixelRatio
   canvas.height = window.innerHeight * devicePixelRatio
 }
+
+render()
+
+window.addEventListener('resize', () => {
+  setSize()
+  render()
+})
+
+canvas.addEventListener('wheel', ({ deltaY: y }) => {
+  scale *= (1 - y / 1000)
+  render()
+})
